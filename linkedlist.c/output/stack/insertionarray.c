d89dcddb5ee69f5452bffc28ef09b756f7b382ec
Toyiba <sheikhtoiba6@gmail.com>
2023-08-06 11:45:17 +0530

    insertion

---
+++ linkedlist.c/output/stack/insertionarray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+
+    int ar[50],i,n,elem;
+    printf("Enter size of Array :");
+    scanf("%d",&n);
+    for(int i=0; i<n; i++){
+        scanf("%d",&ar[i]);
+    }
+    printf("Enter element on Array : ");
+    scanf("%d",&elem)
+    ar[i]=elem;
+    printf("After add element on array");
+    for(int i=0;i<n+1;i++){
+    printf("%d",ar[i])
+
+}
+}
