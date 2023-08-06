2909e9251477fd5f023dc4802cc1a59158c80b92
Toyiba <sheikhtoiba6@gmail.com>
2023-08-06 11:43:10 +0530

    average

---
+++ linkedlist.c/output/stack/array average.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int a[10];
+    int n,i;
+
+    printf("Enter size of array");
+    scanf("%d",n);
+
+    for(int i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+while(int i<n){
+    sum=sum+a[i];
+
+}i++;
+}
+int avg=sum/n;
+printf("%d",avg)
+}
